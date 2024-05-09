7cbd36738b60724b6fb37f5ac01ce10c2fd2be9e
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-05-09 18:49:03 +0300

    add

--- my_project/quotes_by_great_authors/views.py
+++ my_project/quotes_by_great_authors/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-# Create your views here.
+from .utils import get_mongodb
 
-def main(request):
-    return render(request, 'quotes_by_great_authors/index.html', context={})
+def main(request, page=1):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page = paginator.page(page)
+    context = {
+        'quotes': quotes_on_page,
+        'page_number': page,
+    }
+    return render(request, 'quotes_by_great_authors/index.html', context)
 
-#def quotes(request):
- #   return render(request, template_name='quotes_by_great_authors/quotes.html', context={})
-
-#def upload(request):
- #   return render(request, template_name='quotes_by_great_authors/upload.html', context={})
