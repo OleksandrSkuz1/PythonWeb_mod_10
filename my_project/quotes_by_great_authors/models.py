c64988f9266c041a19af7eed8b058f8a7adf4fd3
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2024-05-09 21:07:33 +0300

    add migrations

--- my_project/quotes_by_great_authors/models.py
+++ my_project/quotes_by_great_authors/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    biography = models.TextField(blank=True)
-    description = models.CharField(max_length=300)
+    fullname = models.CharField(max_length=50)
+    born_date = models.DateTimeField(max_length=50)
+    born_location = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30, null=False, unique=True)
+
+
+class Quote(models.Model):
+    quote = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, default=None, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
